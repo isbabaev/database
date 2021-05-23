6d0d7b71ffc436218e750647050c42a18e15ca53
Islam Babaev <48277358+isbabaev@users.noreply.github.com>
2021-05-23 13:39:49 +0300

    Улучшил тест для CreateAccountService

--- src/database/services/__test__/create-account.service.spec.ts
+++ src/database/services/__test__/create-account.service.spec.ts
@@ -1,7 +1,7 @@
 import { Repository } from 'typeorm';
 import { AccountEntity } from '../../entities/account.entity';
 import { CreateAccountService } from '../create-account.service';
-import { capture, instance, mock } from 'ts-mockito';
+import { capture, instance, mock, verify } from 'ts-mockito';
 
 describe('CreateAccountServiceTest', () => {
   let createAccountService: CreateAccountService;
@@ -13,19 +13,10 @@ describe('CreateAccountServiceTest', () => {
   });
 
   test('should call insert method of accountRepository', async () => {
-    const account = new AccountEntity(
-      '123',
-      'Test',
-      'Test',
-      'test@mail.com',
-      'password',
-      new Date('2021-05-22'),
-      new Date('2021-05-22')
-    );
+    const account = mock(AccountEntity);
 
     await createAccountService.createAccount(account);
 
-    const createAccountArguments = capture(accountRepository.insert).first();
-    expect(createAccountArguments[0]).toEqual(account);
+    verify(accountRepository.insert(account)).called();
   });
 });
